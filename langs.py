8ac4a77363fee6ce043d3acc096a17b4ab5ac51b
Carter Temm <cartertemm@gmail.com>
2020-10-26 20:05:13 -0500

    Formatting

--- langs.py
+++ langs.py
@@ -1,28 +1,33 @@
 import os
 
-languages=[]
+languages = []
+
 
 class language:
-
 	def __init__(self, name, extensions, parent_lang="", associated_files=[]):
-		self.name=name
-		self.extensions=extensions
-		self.associated_files=associated_files
-		self.parent_lang=parent_lang
+		self.name = name
+		self.extensions = extensions
+		self.associated_files = associated_files
+		self.parent_lang = parent_lang
 		languages.append(self)
 
 	def __str__(self):
-		final=self.name
+		final = self.name
 		if self.parent_lang:
-			final+=" ("+self.parent_lang+")"
+			final += " (" + self.parent_lang + ")"
 		return final
 
-#here we keep the data for all known languages
-#todo: possibly integrate with a database (linguist) for more accurate results
-#note: the following was mostly generated with a script. I've attempted to clean it up but might have missed something.
+
+# here we keep the data for all known languages
+# todo: possibly integrate with a database (linguist) for more accurate results
+# note: the following was mostly generated with a script. I've attempted to clean it up but might have missed something.
 
 language("AngelScript", [".as", ".angelscript"], "C++")
-language("ApacheConf", [".apacheconf", ".vhost"], associated_files=[".htaccess", "apache2.conf", "httpd.conf"])
+language(
+	"ApacheConf",
+	[".apacheconf", ".vhost"],
+	associated_files=[".htaccess", "apache2.conf", "httpd.conf"],
+)
 language("Apollo Guidance Computer", [".agc"], "assembly_x86")
 language("AppleScript", [".scpt", ".applescript"])
 language("ASP", [".asp", ".aspx", ".asax", ".ascx", ".ashx", ".asmx", ".axd"])
@@ -33,9 +38,9 @@ language("batchfile", [".bat", ".cmd"])
 language("BGT", [".bgt"])
 language("brainfuck", [".bf", ".b"])
 language("C/C++", [".c", ".cpp", ".c++", ".cc", ".cxx", ".h", ".hpp", ".h++", ".hxx"])
-#thanks Ty
+# thanks Ty
 language("C# (CSharp)", [".cs", ".csx", ".cake"])
-#language("chuck", (".ck"))
+# language("chuck", (".ck"))
 language("CSS", [".css"])
 language("CSV", [".csv"])
 language("cython", [".pyx", ".pxd", ".pyi"])
@@ -47,36 +52,173 @@ language("HTML+Django", [".jinja", ".jinja2", ".mustache", ".njk"])
 language("HTML+ERB", [".erb", ".erb.deface"])
 language("HTML+PHP", [".phtml"])
 language("HTTP", [".http"])
-language("INI", [".ini", ".cfg", ".lektorproject", ".prefs", ".pro", ".properties"], associated_files=[".editorconfig", ".gitconfig", "buildozer.spec"])
+language(
+	"INI",
+	[".ini", ".cfg", ".lektorproject", ".prefs", ".pro", ".properties"],
+	associated_files=[".editorconfig", ".gitconfig", "buildozer.spec"],
+)
 language("Inno Setup", [".iss"])
 language("java", [".java"])
 language("java server pages", [".jsp"])
-language("javascript", [".js", "._js", ".bones", ".es", ".es6", ".frag", ".gs", ".jake", ".jscad", ".jsfl", ".jsm", ".mjs", ".njs", ".pac", ".sjs", ".ssjs", ".xsjs", ".xsjslib"])
+language(
+	"javascript",
+	[
+		".js",
+		"._js",
+		".bones",
+		".es",
+		".es6",
+		".frag",
+		".gs",
+		".jake",
+		".jscad",
+		".jsfl",
+		".jsm",
+		".mjs",
+		".njs",
+		".pac",
+		".sjs",
+		".ssjs",
+		".xsjs",
+		".xsjslib",
+	],
+)
 language("jaws script", [".jsb", ".jss"])
 language("LOLCode", [".lol"])
 language("Linux Kernel Module", [".mod"])
 language("lua", [".lua", ".wlua"])
-language("makefile", [".mak", ".make", ".mk", ".mkfile"], associated_files=["BSDmakefile", "GNUmakefile", "Kbuild", "Makefile", "Makefile.am", "Makefile.boot", "Makefile.frag", "Makefile.in", "Makefile.inc", "Makefile.wat", "makefile", "makefile.sco", "mkfile"])
-language("markdown", [".md", ".markdown", ".mdown", ".mdwn", ".mkd", ".mkdn", ".mkdown", ".ronn", ".workbook"], parent_lang="text")
+language(
+	"makefile",
+	[".mak", ".make", ".mk", ".mkfile"],
+	associated_files=[
+		"BSDmakefile",
+		"GNUmakefile",
+		"Kbuild",
+		"Makefile",
+		"Makefile.am",
+		"Makefile.boot",
+		"Makefile.frag",
+		"Makefile.in",
+		"Makefile.inc",
+		"Makefile.wat",
+		"makefile",
+		"makefile.sco",
+		"mkfile",
+	],
+)
+language(
+	"markdown",
+	[
+		".md",
+		".markdown",
+		".mdown",
+		".mdwn",
+		".mkd",
+		".mkdn",
+		".mkdown",
+		".ronn",
+		".workbook",
+	],
+	parent_lang="text",
+)
 language("moo", [".moo"])
 language("Nmap Scripting Engine", [".nse"], parent_lang="lua")
 language("NSIS", [".nsi", ".nsh"])
 language("Nginx", [".nginxconf"], associated_files=["nginx.conf"])
 language("PHP", [".php", ".php3", ".php4", ".php5", ".phps", ".phpt"])
-language("Perl", [".pl", ".al", ".perl", ".plx", ".pm"], associated_files=["Makefile.PL", "rexfile", "ack", "cpanfile"])
+language(
+	"Perl",
+	[".pl", ".al", ".perl", ".plx", ".pm"],
+	associated_files=["Makefile.PL", "rexfile", "ack", "cpanfile"],
+)
 language("Powershell", [".ps1", ".psd1", ".psm1"])
 language("PureBasic", [".pb", ".pbi"])
-language("python", [".py", ".pyw", ".py2", ".py3", ".pyi", ".pip"], associated_files=[".gclient", "BUCK", "BUILD", "BUILD.bazel", "SConscript", "SConstruct", "Snakefile", "WORKSPACE", "wscript"])
+language(
+	"python",
+	[".py", ".pyw", ".py2", ".py3", ".pyi", ".pip"],
+	associated_files=[
+		".gclient",
+		"BUCK",
+		"BUILD",
+		"BUILD.bazel",
+		"SConscript",
+		"SConstruct",
+		"Snakefile",
+		"WORKSPACE",
+		"wscript",
+	],
+)
 language("Regular Expression", [".regexp", ".regex"])
-language("ruby", [".rb", ".builder", ".eye", ".gemspec", ".god", ".jbuilder", ".mspec", ".pluginspec", ".podspec", ".rabl", ".rake", ".rbuild", ".rbw", ".rbx", ".ruby", ".thor", ".watchr"], associated_files=[".irbrc", ".pryrc", "Appraisals", "Berksfile", "Brewfile", "Buildfile", "Capfile", "Dangerfile", "Deliverfile", "Fastfile", "Gemfile", "Gemfile.lock", "Guardfile", "Jarfile", "Mavenfile", "Podfile", "Puppetfile", "Rakefile", "Snapfile", "Thorfile"])
+language(
+	"ruby",
+	[
+		".rb",
+		".builder",
+		".eye",
+		".gemspec",
+		".god",
+		".jbuilder",
+		".mspec",
+		".pluginspec",
+		".podspec",
+		".rabl",
+		".rake",
+		".rbuild",
+		".rbw",
+		".rbx",
+		".ruby",
+		".thor",
+		".watchr",
+	],
+	associated_files=[
+		".irbrc",
+		".pryrc",
+		"Appraisals",
+		"Berksfile",
+		"Brewfile",
+		"Buildfile",
+		"Capfile",
+		"Dangerfile",
+		"Deliverfile",
+		"Fastfile",
+		"Gemfile",
+		"Gemfile.lock",
+		"Guardfile",
+		"Jarfile",
+		"Mavenfile",
+		"Podfile",
+		"Puppetfile",
+		"Rakefile",
+		"Snapfile",
+		"Thorfile",
+	],
+)
 language("rust", [".rs", ".rs.in"])
-language("shell script", [".sh", ".bash", ".ksh", ".sh.in", ".tmux", ".zsh"], associated_files=[".bash_logout", ".bash_profile", ".bashrc", ".login", ".profile", ".zlogin", ".zlogout", ".zprofile", ".zshenv", ".zshrc"])
+language(
+	"shell script",
+	[".sh", ".bash", ".ksh", ".sh.in", ".tmux", ".zsh"],
+	associated_files=[
+		".bash_logout",
+		".bash_profile",
+		".bashrc",
+		".login",
+		".profile",
+		".zlogin",
+		".zlogout",
+		".zprofile",
+		".zshenv",
+		".zshrc",
+	],
+)
 language("swift", [".swift"])
-language("visual basic", [".vb", ".bas", ".cls", ".frm", ".frx", ".vba", ".vbhtml", ".vbs"])
+language(
+	"visual basic", [".vb", ".bas", ".cls", ".frm", ".frx", ".vba", ".vbhtml", ".vbs"]
+)
 language("windows registry entry", [".reg"])
 
+
 def detect_lang(filename):
-	file_ext=os.path.splitext(filename)[1]
+	file_ext = os.path.splitext(filename)[1]
 	for i in languages:
 		if file_ext in i.extensions:
 			return i
--- stats.py
+++ stats.py
@@ -5,18 +5,20 @@ import argparse
 import langs
 import string
 
+
 def lstrip(s):
 	"""strip whitespace from the left hand side of a string, returning a tuple (string,	number_of_chars_stripped) for S"""
-	num_stripped=0
+	num_stripped = 0
 	for i in s:
-		#for bytes
-		char=(chr(i) if type(i)==int else i)
+		# for bytes
+		char = chr(i) if type(i) == int else i
 		if char in string.whitespace:
-			s=s[1:]
-			num_stripped+=1
+			s = s[1:]
+			num_stripped += 1
 		else:
 			break
 	return (s, num_stripped)
+
 
 def recursive(directory, wildcard):
 	matches = []
@@ -25,24 +27,49 @@ def recursive(directory, wildcard):
 			matches.append(os.path.join(root, filename))
 	return matches
 
+
 def bytes2human(n):
-	symbols = ('KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
+	symbols = ("KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
 	prefix = {}
 	for i, s in enumerate(symbols):
 		prefix[s] = 1 << (i + 1) * 10
 	for s in reversed(symbols):
 		if n >= prefix[s]:
 			value = float(n) / prefix[s]
-			return '%.1f%s' % (value, s)
+			return "%.1f%s" % (value, s)
 	return "%sB" % n
 
-parser=argparse.ArgumentParser(description="Tool to provide detailed summaries of code in a project.")
+
+parser = argparse.ArgumentParser(
+	description="Tool to provide detailed summaries of code in a project."
+)
 parser.add_argument("dir", help="directory to search")
-parser.add_argument("-e", "--exclude", dest="excludes", action="append", default=[], help="adds a wildcard exclusion")
-parser.add_argument("-ef", "--exclude-file", help="Path to a file containing a list of excludes to be processed seperated by \\n")
-parser.add_argument("-v", "--verbose", action="store_true", help="give args.verbose/more detailed output")
-parser.add_argument("-dr", "--disable-rundown", action="store_true", help="excludes an individual rundown of every processed file")
-args=parser.parse_args()
+parser.add_argument(
+	"-e",
+	"--exclude",
+	dest="excludes",
+	action="append",
+	default=[],
+	help="adds a wildcard exclusion",
+)
+parser.add_argument(
+	"-ef",
+	"--exclude-file",
+	help="Path to a file containing a list of excludes to be processed seperated by \\n",
+)
+parser.add_argument(
+	"-v",
+	"--verbose",
+	action="store_true",
+	help="give args.verbose/more detailed output",
+)
+parser.add_argument(
+	"-dr",
+	"--disable-rundown",
+	action="store_true",
+	help="excludes an individual rundown of every processed file",
+)
+args = parser.parse_args()
 
 if not os.path.isdir(args.dir):
 	print("error, invalid directory provided")
@@ -52,96 +79,115 @@ if args.verbose:
 if args.exclude_file and os.path.isfile(args.exclude_file):
 	if args.verbose:
 		print(f"found exclude file: {args.exclude_file}")
-	f=open("excludes.txt", "r")
-	excludes_txt=f.read().split("\n")
+	f = open("excludes.txt", "r")
+	excludes_txt = f.read().split("\n")
 	f.close()
-	excludes_txt=[i for i in excludes_txt if i]
-	args.excludes+=excludes_txt
+	excludes_txt = [i for i in excludes_txt if i]
+	args.excludes += excludes_txt
 	if args.verbose:
 		print(f"contains {len(excludes_txt)} exclusions")
 
 if args.verbose:
-	print("loaded a total of "+str(len(args.excludes))+" exclusion"+("s" if len(args.excludes)!=1 else ""))
+	print(
+		"loaded a total of "
+		+ str(len(args.excludes))
+		+ " exclusion"
+		+ ("s" if len(args.excludes) != 1 else "")
+	)
 
-total_lines=0
-total_whitespace=0
-total_size =0
-total_indents=0
-line_amounts=[0, 0]
-files={}
-directory=recursive(args.dir, "*")
+total_lines = 0
+total_whitespace = 0
+total_size = 0
+total_indents = 0
+line_amounts = [0, 0]
+files = {}
+directory = recursive(args.dir, "*")
 if args.verbose:
 	print(f"{len(args.dir)} files found in directory, now scanning...")
-num_excluded=0
+num_excluded = 0
 for i in directory:
-	#os.path.splitext only returns the last component
-	ext=i.split("/")[-1]
-	ext=ext[ext.find("."):]
-	exclude=False
+	# os.path.splitext only returns the last component
+	ext = i.split("/")[-1]
+	ext = ext[ext.find(".") :]
+	exclude = False
 	if i in args.excludes:
-		exclude=True
+		exclude = True
 	else:
 		for ex in args.excludes:
 			if fnmatch.fnmatch(i, ex):
-				exclude=True
+				exclude = True
 				break
 	if exclude:
 		if args.verbose:
-			print("skipping: "+i+" due to exclude")
-		num_excluded+=1
+			print("skipping: " + i + " due to exclude")
+		num_excluded += 1
 		continue
-	lang=langs.detect_lang(i)
+	lang = langs.detect_lang(i)
 	if not lang:
 		if args.verbose:
-			print("unknown language from file "+i+". No info will be provided. Continuing")
+			print(
+				"unknown language from file "
+				+ i
+				+ ". No info will be provided. Continuing"
+			)
 		continue
-	f=open(i, "rb")
-	content=f.read()
-	size=len(content)
-	total_size+=size
-	lines=content.split(b"\n")
-	total_lines+=len(lines)
-	whitespace=0
-	indent=0
+	f = open(i, "rb")
+	content = f.read()
+	size = len(content)
+	total_size += size
+	lines = content.split(b"\n")
+	total_lines += len(lines)
+	whitespace = 0
+	indent = 0
 	for line in lines:
-		if line==b"" or line==b"\r":
-			whitespace+=1
+		if line == b"" or line == b"\r":
+			whitespace += 1
 		else:
-			line_amounts[0]+=1
-			line_amounts[1]+=len(line)
-		indent+=lstrip(line)[1]
-	total_indents+=indent
-	total_whitespace+=whitespace
+			line_amounts[0] += 1
+			line_amounts[1] += len(line)
+		indent += lstrip(line)[1]
+	total_indents += indent
+	total_whitespace += whitespace
 	f.close()
-	files[i]=[lang, len(lines), size, indent, whitespace]
+	files[i] = [lang, len(lines), size, indent, whitespace]
 
 print(f"{len(files)} files accounted for, with {num_excluded} exclusions encountered")
-if len(files)==0:
+if len(files) == 0:
 	print("no stats to show")
 	sys.exit()
-#calculate percentages per language
-lang_amounts={}
+# calculate percentages per language
+lang_amounts = {}
 for i in files.values():
 	if i[0] in lang_amounts:
-		lang_amounts[i[0]]+=i[2]
+		lang_amounts[i[0]] += i[2]
 	else:
-		lang_amounts[i[0]]=i[2]
+		lang_amounts[i[0]] = i[2]
 for i in lang_amounts:
 	print(f"{i}: {round((lang_amounts[i]/total_size)*100, 2)}%")
 print(f"total lines: {total_lines}")
 print(f"number of blank lines: {total_whitespace}")
 print(f"total lines that have content: {total_lines-total_whitespace}")
-print(f"average number of characters per line: {round(line_amounts[1]/line_amounts[0], 2)}")
+print(
+	f"average number of characters per line: {round(line_amounts[1]/line_amounts[0], 2)}"
+)
 print(f"average number of lines per file: {round(total_lines/len(files), 2)}")
-print(f"total number of proceeding spaces/indents: {total_indents}, making up a total of {bytes2human(total_indents)}")
+print(
+	f"total number of proceeding spaces/indents: {total_indents}, making up a total of {bytes2human(total_indents)}"
+)
 print(f"average indents per file: {round(total_indents/len(files), 2)}")
 print(f"total size: {bytes2human(total_size)}, {total_size}b")
-without_indents=total_size-total_indents
+without_indents = total_size - total_indents
 print(f"total size without indents: {bytes2human(without_indents)}, {without_indents}b")
-print(f"average file size: {bytes2human(total_size/len(files))}, {round(total_size/len(files), 2)}b")
-print(f"average file size without indents: {bytes2human(without_indents/len(files))}, {round(without_indents/len(files), 2)}b")
+print(
+	f"average file size: {bytes2human(total_size/len(files))}, {round(total_size/len(files), 2)}b"
+)
+print(
+	f"average file size without indents: {bytes2human(without_indents/len(files))}, {round(without_indents/len(files), 2)}b"
+)
 
 if not args.disable_rundown:
 	print("individual rundown:")
 	for i in files:
-		print(f"{i}, {files[i][0].name}, {files[i][1]} lines, {bytes2human(files[i][2])}, {files[i][2]}b")
+		print(
+			f"{i}, {files[i][0].name}, {files[i][1]} lines, {bytes2human(files[i][2])}, {files[i][2]}b"
+		)
